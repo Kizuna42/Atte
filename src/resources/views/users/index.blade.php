f16f86ef4afcf76d52cd6fc40a08186cef0a9fbd
Kizuna <kizunapower777@icloud.com>
2024-11-14 23:35:21 +0900

    finish

--- src/resources/views/users/index.blade.php
+++ src/resources/views/users/index.blade.php
@@ -7,7 +7,7 @@
     <ul>
         @foreach($users as $user)
             <li>
-                <a href="{{ route('users.show', $user) }}">{{ $user->name }}</a>
+                <a href="{{ route('users.show', $user) }}" class = "user">{{ $user->name }}</a>
             </li>
         @endforeach
     </ul>
